be070d150d275fd54a150bb5adb63e46a2ad3406
rseyum <103169939+rseyum@users.noreply.github.com>
2022-04-29 22:06:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -210,10 +210,10 @@ console.log('A' + 1);//A1
 console.log(2 + '2' + '2');//222
 console.log('hello' + 'world' + 89);//helloworld89
 console.log('hello' - 'world' + 89);//NaN
-// console.log('hello' + 78);// hello78
-// console.log('78' - 90 + '2');// -122
-// console.log(2 - '2' + 90);// 90
-// console.log(89 - '90' / 2);//44
+console.log('hello' + 78);// hello78
+console.log('78' - 90 + '2');// -122
+console.log(2 - '2' + 90);// 90
+console.log(89 - '90' / 2);//44
 
 
 
