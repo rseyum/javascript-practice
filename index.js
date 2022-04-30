d3a194c331d3603e185cd90e22345c3d9c7af77e
rseyum <103169939+rseyum@users.noreply.github.com>
2022-04-29 22:05:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -205,11 +205,11 @@ num=num+1;
 
 // Implicit coercion:
 //practice questions on implicit coersion
-// console.log('A' - 1);//NaN
-// console.log('A' + 1);//A1
-// console.log(2 + '2' + '2');//222
-// console.log('hello' + 'world' + 89);//helloworld89
-// console.log('hello' - 'world' + 89);//NaN
+console.log('A' - 1);//NaN
+console.log('A' + 1);//A1
+console.log(2 + '2' + '2');//222
+console.log('hello' + 'world' + 89);//helloworld89
+console.log('hello' - 'world' + 89);//NaN
 // console.log('hello' + 78);// hello78
 // console.log('78' - 90 + '2');// -122
 // console.log(2 - '2' + 90);// 90
